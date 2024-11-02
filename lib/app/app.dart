72d0e42852a13ff51458ecb253ff11a5588e6eb0
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2024-11-02 14:19:12 +0200

    navigate using future.delayed

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:to_do_app/core/utils/app_assets.dart';
-import 'package:to_do_app/core/utils/app_colors.dart';
 import 'package:to_do_app/core/utils/app_strings.dart';
+import 'package:to_do_app/features/auth/presentation/screens/splash_screen/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: AppStrings.appName,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: AppColors.background,
-        body: Center(
-          child: SvgPicture.asset(AppAssets.logo),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
